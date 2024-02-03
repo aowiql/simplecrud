eba9d1d130ec8779668f5198f936ddcad8a24607
qweewq <seongkh15@naver.com>
2024-02-03 16:10:17 +0900

    detail -> DELETE

--- client/src/components/DoneDetail/DoneDetail.tsx
+++ client/src/components/DoneDetail/DoneDetail.tsx
@@ -1,14 +1,46 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useMutation, useQueryClient } from "react-query";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
+import { deleteBackend } from "../../api/deletePost";
 
 const DoneDetail = () => {
   const { id } = useParams();
   const location = useLocation();
   const boardPost = location.state?.boardPost || "";
 
+  const navigate = useNavigate();
+
+  const goDonePage = () => {
+    navigate("/donepage");
+  };
+
+  const backUrl = "http://localhost:8080";
+
+  const queryClient = useQueryClient();
+
+  const mutation = useMutation(
+    (postId: number) => deleteBackend(backUrl, postId),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("posts");
+        navigate("/donepage");
+      },
+    }
+  );
+
+  const deleteHandler = async (postId: number) => {
+    try {
+      await mutation.mutateAsync(postId);
+    } catch (error) {
+      console.error("Error", error);
+    }
+  };
+
   return (
     <>
       <h1>test {id}</h1>
       <p>{boardPost}</p>
+      <button onClick={() => deleteHandler(Number(id))}>삭제</button>
+      <button onClick={goDonePage}>뒤로가기</button>
     </>
   );
 };
