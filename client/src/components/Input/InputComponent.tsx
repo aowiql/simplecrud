7c5d37cf4bb5d1b84eabd9836685175f518036b0
qweewq <seongkh15@naver.com>
2024-02-03 12:33:14 +0900

    input page

--- client/src/components/Input/InputComponent.tsx
+++ client/src/components/Input/InputComponent.tsx
@@ -1,11 +1,25 @@
-import "./InputComponent";
+import { useNavigate } from "react-router-dom";
+import "./InputComponent.css";
 
 const InputComponent = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
-      <input />
-      <textarea className="boardPost"></textarea>
-      <button>입력</button>
+    <div className="inputPage">
+      <div className="titleInput">
+        <input placeholder="제목" />
+      </div>
+      <div className="postInput">
+        <textarea placeholder="본문" className="boardPost"></textarea>
+      </div>
+      <div className="inputBtn">
+        <button onClick={goBack}>이전</button>
+        <button>입력</button>
+      </div>
     </div>
   );
 };
